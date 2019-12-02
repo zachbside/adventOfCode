60fb9ddd675eb242899d183cedf41e80b7464e3c
zachbside <53321834+zachbside@users.noreply.github.com>
2019-12-02 14:05:52 -0500

    Delete day1_1.ps1

--- day1_1.ps1
+++
@@ -1,7 +0,0 @@
-$fuelSum = 0
-[decimal[]]$inputs = Get-Content -Path .\input.txt
-for ($i=0;$i -le $inputs.Length-1;$i++) {
-    $inputs[$i] = [math]::Floor($inputs[$i]/3)-2
-    $fuelSum += $inputs[$i]
-}
-$fuelSum
