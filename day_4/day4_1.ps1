7fe07b127f91b2a55f0d0409b7c3280894b4967d
zachbside <53321834+zachbside@users.noreply.github.com>
2019-12-12 14:41:30 -0500

    Create day4_1.ps1

---
+++ day_4/day4_1.ps1
@@ -0,0 +1,44 @@
+[int[]]$start = 3,7,2,0,3,7
+[int[]]$end = 9,0,5,1,5,7
+
+[int]$startNum=[string]$start[0]+$start[1]+$start[2]+$start[3]+$start[4]+$start[5]
+[int]$endNum=[string]$end[0]+$end[1]+$end[2]+$end[3]+$end[4]+$end[5]
+
+$iterations= $endNum-$startNum
+$correctPasswords = 0
+
+function Is-Doubled ($arrayInput) {
+    for($i=0;$i -lt $arrayInput.Count; $i++) {
+        if($arrayInput[$i] -eq $arrayInput[$i+1]) {
+            Return $True
+        }
+    }
+    Return $False
+}
+
+function Is-Increasing ($arrayInput) {
+    for($i=0;$i -lt $arrayInput.Count-1; $i++) {
+        if($arrayInput[$i] -gt $arrayInput[$i+1]) {
+            Return $False
+        }
+    }
+    Return $True
+}
+
+function Increase-Number ($arrayInput) {
+    $arrayInput[5]+=1
+    for($i=$arrayInput.Count-1;$i -gt 0;$i--) {
+        if ($arrayInput[$i] -eq 10) {
+            $arrayInput[$i] = 0
+            $arrayInput[$i-1]+=1
+        }
+    }
+}
+
+for($i=0;$i -lt $iterations;$i++) {
+    if((Is-Doubled($start) -eq $True) -AND (Is-Increasing($start) -eq $True)) {
+        $correctPasswords++
+    }
+    Increase-Number($start)
+}
+$correctPasswords
